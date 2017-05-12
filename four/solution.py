d712f07dfbe70b03c9cf88192ceafa78bfc81895
stevenus <shuggard.us@gmail.com>
2017-05-11 21:44:14 -0700

    minor change to solution

--- four/solution.py
+++ four/solution.py
@@ -9,7 +9,7 @@ board1 = [
 board2 = [
             [0,0,0,0,0,0],
             [0,0,0,0,0,0],
-            [0,1,1,1,0,0],
+            [1,1,1,1,0,0],
             [0,0,0,0,0,0],
             [0,0,0,0,0,0],
             [0,0,0,0,0,0]
@@ -30,12 +30,12 @@ print("up dwn: ", up_dwn_total)
 
 def check_right(board, piece, idx1, idx2):
     if idx2+1 < len(board[0]) and board[idx1][idx2+1] == piece:
-        return 1 + check_dwn(board, piece, idx1+1, idx2)
+        return 1 + check_right(board, piece, idx1, idx2+1)
     return 0
 def check_left(board, piece, idx1, idx2):
     if idx2-1 > -1 and board[idx1][idx2-1] == piece:
-        return 1 + check_up(board, piece, idx1-1, idx2)
+        return 1 + check_left(board, piece, idx1, idx2-1)
     return 0
 
-l_r_total = 1 + check_left(board1, 1, 2, 1) + check_right(board1, 1, 2, 1)
+l_r_total = 1 + check_left(board2, 1, 2, 1) + check_right(board2, 1, 2, 1)
 print("L R: ", l_r_total)
